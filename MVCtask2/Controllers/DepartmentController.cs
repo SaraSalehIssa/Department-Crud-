91df65be5cf537a69728158fcc9fd3e3f3cda399
SaraIssa <issasalehsara2001@gmail.com>
2024-05-08 23:41:58 +0300

    CRUD Task

---
+++ MVCtask2/Controllers/DepartmentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCtask2.Data;
+using MVCtask2.Models;
+
+namespace MVCtask2.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var departments = context.Departments.ToList();
+            return View("Index", departments);
+        }
+        // To show details about each Department
+        public IActionResult Details(int id)
+        {
+            var department = context.Departments.Find(id);
+            return View("Details", department);
+        }
+
+        // To create new Department
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        public IActionResult Add(Department department)
+        {
+            context.Departments.Add(department);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // To remove Department
+        public IActionResult Delete(int id)
+        {
+            var department = context.Departments.Find(id);
+
+            context.Departments.Remove(department);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCtask2/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCtask2.Models;
+
+namespace MVCtask2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=MVC_Department_CRUD;trusted_connection=true;trustServerCertificate=true;");
+        }
+    }
+}
